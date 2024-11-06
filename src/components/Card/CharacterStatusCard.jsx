d47de43447aca6b17b865fe070718e28f75950ba
ma21005 <hockey0513hockey@gmail.com>
2024-11-06 21:49:00 +0900

    リファクタ

--- src/components/Card/CharacterStatusCard.jsx
+++ src/components/Card/CharacterStatusCard.jsx
@@ -17,15 +17,9 @@ export default function CharacterStatusCard({ character }) {
   return (
     <CardContainer>
       <h2 className="text-lg font-bold">{character.名前}</h2>
-      <StatusGraph status={status}/>
-      <p>トリオン: {character.トリオン}</p>
-      <p>攻撃力: {character.攻撃}</p>
-      <p>防御・援護: {character["防御・援護"]}</p>
-      <p>機動: {character.機動}</p>
-      <p>技術: {character.技術}</p>
-      <p>射程: {character.射程}</p>
-      <p>指揮: {character.指揮}</p>
-      <p>特殊戦術: {character.特殊戦術}</p>
+      <div className="h-[80%]">
+        <StatusGraph status={status}/>
+      </div>
     </CardContainer>
   );
 }
--- src/components/Card/CharacterTitleCard.jsx
+++ src/components/Card/CharacterTitleCard.jsx
@@ -7,13 +7,14 @@ export default function CharacterTitleCard({ character }) {
   return (
     <CardContainer>
       <h2 className="text-lg font-bold">{character.名前}</h2>
-      <img
-        src={`https://borders-records.s3.ap-northeast-1.amazonaws.com/characters/${character.名前}.png`}
-        alt={`${character.名前}`}
-        className="w-3/4 rounded-xl"
-        style={{ backgroundColor: characterBackGroudColor }}
-      />
-      <p>ポジション: {character.ポジション}</p>
+      <div className="h-[80%] rounded-xl">
+        <img
+          src={`https://borders-records.s3.ap-northeast-1.amazonaws.com/characters/${character.名前}.png`}
+          alt={`${character.名前}`}
+          style={{ backgroundColor: characterBackGroudColor }}
+          className="object-cover w-full h-full"
+        />
+      </div>
     </CardContainer>
   );
 }

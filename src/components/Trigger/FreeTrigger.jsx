7f06ae5f8ab709ef320a248314c19f0b9126afd2
ma21005 <hockey0513hockey@gmail.com>
2024-12-16 21:49:30 +0900

    FREE TRIGGER デザイン修正

--- src/components/Trigger/FreeTrigger.jsx
+++ src/components/Trigger/FreeTrigger.jsx
@@ -1,8 +1,8 @@
-export default function MainTrigger({ trigger }) {
+export default function FreeTrigger() {
   return (
-    <div className="flex w-full bg-gray-500 mb-1">
-      <div className="h-11 m-1 bg-gray-500">
-        <h2>FREE TRIGGER</h2>
+    <div className="flex w-full bg-[#333333] mb-1">
+      <div className="h-11 m-1 flex items-center mx-auto">
+        <h2 className="font-bold">FREE TRIGGER</h2>
       </div>
     </div>
   );
